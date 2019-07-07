b39dc5b808f08b7167c76547dd700220967962f5
MaryamSadegh <51027439+MaryamSadegh@users.noreply.github.com>
2019-07-07 15:10:12 -0700

    Project 1

--- Plot2.R
+++ Plot2.R
@@ -1,4 +1,5 @@
-dataFile <- "./data/household_power_consumption.txt"
+rm(list=ls())
+data <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?")
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
