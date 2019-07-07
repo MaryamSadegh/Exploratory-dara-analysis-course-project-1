e7ebedec397a64a520051f284bbb2bfcaf8585c0
MaryamSadegh <51027439+MaryamSadegh@users.noreply.github.com>
2019-07-07 15:10:50 -0700

    project 1

--- Plot3.R
+++ Plot3.R
@@ -1,5 +1,5 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+rm(list=ls())
+data <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
